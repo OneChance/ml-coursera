7553152b506f391b2c11b5280c0b564287033e56
OneChance <zhouh08@hotmail.com>
2017-08-28 14:25:18 +0800

    week 5 complete

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -83,17 +83,31 @@ J =  -(1/m)*(sum((log(sigmoid(a2*Theta2')).*ym)(:))+sum((log(1-sigmoid(a2*Theta2
       (lambda/(2*m))*(sum((Theta1(:,[2:theta1Col]).^2)(:))+sum((Theta2(:,[2:theta2Col]).^2)(:)));
 
 
+%后向传播
+d1Sum = zeros(size(Theta1)(1),size(Theta1)(2));     
+d2Sum = zeros(size(Theta2)(1),size(Theta2)(2));
 
+for t=1:m
+    fa1 = X(t,:);
+    
+    fa2 = sigmoid(fa1*Theta1');
+    fa2 = [ones(size(fa2, 1), 1) fa2];
+    fa3 = sigmoid(fa2*Theta2');  %预测结果
 
+    d3 = fa3 - ym(t,:);
+    d2 = (Theta2'*d3').*fa2'.*(1-fa2)'; 
+    d2 = d2(2:size(d2)(1));%d2的计算和课程公式略有出入,主要是行向量和列向量的差别,最终
+    %d2将会是一个n维向量,n是第二层神经元个数（去掉偏置神经元）
+    
+    d2Sum = d2Sum + d3'*fa2;
+    d1Sum = d1Sum + d2*fa1;
+end;
 
+Theta1_grad(:,1) = (1/m)*(d1Sum(:,1));
+Theta1_grad(:,[2:size(Theta1_grad)(2)]) = (1/m)*(d1Sum(:,[2:size(d1Sum)(2)])+lambda*Theta1(:,[2:theta1Col]));
 
-
-
-
-
-
-
-
+Theta2_grad(:,1) = (1/m)*(d2Sum(:,1));
+Theta2_grad(:,[2:size(Theta2_grad)(2)]) = (1/m)*(d2Sum(:,[2:size(d2Sum)(2)])+lambda*Theta2(:,[2:theta2Col]));
 
 
 
